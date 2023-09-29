6ce0f4cb5a4819977ae4493d284155b429be5458
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-29 23:42:57 +0000

    Add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,27 @@
-import '@/styles/globals.css'
+import Navbar from '@/components/Navbar';
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
   title: 'Connectit',
-  description: 'Unleash the Power of Online Community. Connecting people through vibrant discussions, diverse perspectives, and endless possibilities. Join the conversation and experience a digital community like no other.',
-}
+  description:
+    'Unleash the Power of Online Community. Connecting people through vibrant discussions, diverse perspectives, and endless possibilities. Join the conversation and experience a digital community like no other.',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
